fb3c5e24c24ee79e66763952fa8a4d484e9c6f38
adalid.bori <adalid.bori@TEPC-15>
2023-01-05 16:33:53 -0500

    pantalla de Register creada

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,6 +1,49 @@
+import { useState } from 'react';
 import styles from './Register.module.css';
+import classnames from 'classnames';
 
 export function Register() {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function isDisable(){
+        if(!error){
+            if(username){//preguntar si no es vacio o falso
+                if(password){
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }else{
+                return true;
+            }
+        }else{
+            return true;
+        }
+    }
+
+    function validateEmail(email) {
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+    }
+
+    function handleChange(event) {
+        const value = event.target.value;
+        setEmail(value);
+        if (!validateEmail(value)) {
+            setError(true);
+        } else {
+            setError(false);
+        }
+    }
+
+    const classes = classnames(styles.inputEmail, {
+        [styles.inputEmailError]: error
+    });
+
     return (
         <div>
             <h1 className={styles.title}>Create your account</h1>
@@ -9,18 +52,20 @@ export function Register() {
             <div className={styles.generalContainer}>
                 <div className={styles.usernameContainer}>
                     <label className={styles.usernameLabel} htmlFor="idUsername">digital.profile/</label>
-                    <input className={styles.inputUsername} type="text" id='idUsername' placeholder="username" />
+                    <input className={styles.inputUsername} type="text" id='idUsername' placeholder="username" onChange={e => setUsername(e.target.value)} />
                 </div>
             </div>
 
             <div className={styles.inputEmailConteiner}>
-                <input className={styles.inputEmail} type="email" name="" id="" placeholder="Email" />
+                <input className={classes} type="email" name="" id="" placeholder="Email" onChange={handleChange} />
+                
+                {<p style={{ color: 'red' }}>{error}</p>}
             </div>
-            <div>
-                <input type="password" name="" id="" placeholder="Password" />
+            <div className={styles.inputEmailConteiner}>
+                <input className={styles.inputEmail} type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
             </div>
-            <div>
-                <button>Create account</button>
+            <div className={styles.buttonCreateContainer}>
+                <button className={styles.buttonCreate} disabled={isDisable()}>Create account</button>
             </div>
         </div>
     );
