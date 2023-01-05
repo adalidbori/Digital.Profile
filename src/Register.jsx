85e9b4b3cfb9c0118811319a72f6af9be0cae4a2
adalid.bori <adalid@Adalids-MacBook-Air.local>
2023-01-04 22:06:25 -0500

    input disign

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,21 +1,23 @@
 import styles from './Register.module.css';
 
-export function Register(){
-    return(
+export function Register() {
+    return (
         <div>
             <h1 className={styles.title}>Create your account</h1>
             <p className={styles.smallDesc}>Choose your Linktree username. You can always change it later.</p>
-            
-            <div className={styles.usernameContainer}>
-                <label className={styles.usernameLabel} htmlFor="idUsername">digital.profile/</label>
-                <input className={styles.inputUsername} type="text" id='idUsername' placeholder="Username"/>
+
+            <div className={styles.generalContainer}>
+                <div className={styles.usernameContainer}>
+                    <label className={styles.usernameLabel} htmlFor="idUsername">digital.profile/</label>
+                    <input className={styles.inputUsername} type="text" id='idUsername' placeholder="username" />
+                </div>
             </div>
-            
-            <div>
-                <input type="email" name="" id="" placeholder="Email" />
+
+            <div className={styles.inputEmailConteiner}>
+                <input className={styles.inputEmail} type="email" name="" id="" placeholder="Email" />
             </div>
             <div>
-                <input type="password" name="" id="" placeholder="Password"/>
+                <input type="password" name="" id="" placeholder="Password" />
             </div>
             <div>
                 <button>Create account</button>
